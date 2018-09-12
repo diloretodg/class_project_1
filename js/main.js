12d18ee292d1ed14cb13d9fb081390162e2b3090
ANS NEOphyte <jpbrown1023@gmail.com>
2018-09-12 19:58:42 -0400

    youtube link added, css updates, js-added firebase; removed loop, just plays tone now; pushes tone to firebase

--- js/main.js
+++ js/main.js
@@ -1,19 +1,46 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyChswfB5HAG1cQcjkai_26cvtFHtYqpNYU",
+authDomain: "project1-a46e9.firebaseapp.com",
+databaseURL: "https://project1-a46e9.firebaseio.com",
+projectId: "project1-a46e9",
+storageBucket: "project1-a46e9.appspot.com",
+messagingSenderId: "1004479758001"
+};
+firebase.initializeApp(config);
+
+// Played Notes Array
 var noteIntervals = [];
-// test song function
 
+// Song Notes Array
+var musicNotes = [];
 
+// on Click button for adding notes to noteIntervals Array
+$(".music-note").on("click", function(event) {
+    event.preventDefault();
+    var grabId = this.id;
+    noteIntervals.push(grabId);
+    console.log(noteIntervals);
 
-
-
-
-
+// Creates temp local object for holding note info
+// Depends on what we're going to save to Firebase
+    var savedNotes = {
+      
+    };
+  
+    // Uploads user's played notes to the database
+    // IF we got with the local object, that would go in noteIntervals place
+    database.ref().push(noteIntervals);
+      // Logs everything to console
+    console.log(noteIntervals);
 
 // handles on click note plays acts like a loop should be in other file to turn on off.
 $(document).on("click", ".music-note", function(){
     var note = $(this).attr("data-note");
     play(note);
     console.log("playing "+ note);   
-    noteIntervals.push(setInterval(function() {play(note) }, 2000))});
+    noteIntervals.push(note);
+    });
 
 
 // clear all playing notes
@@ -35,5 +62,7 @@ function play(n) {
 
     console.log("playing "+ n + " audio file")
     console.log(audio)
-};     
+};
+})     
+
  
